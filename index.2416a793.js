92b7df143908bb917230bda923b5d642f8ff332f
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-07-05 20:39:02 -0300

    Updates

---
+++ index.2416a793.js
@@ -0,0 +1,2 @@
+let e=0;const a=setInterval((()=>{e++,console.log("hola"+e),e>5&&clearInterval(a)}),1e3);export{$a4018e003ec9cbe06ab0cc0137aa4e69$exports as default};
+//# sourceMappingURL=index.2416a793.js.map
